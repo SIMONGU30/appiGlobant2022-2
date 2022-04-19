011fa6ac74e86a0569881b69917b87ed1b3a72e7
saimon <simonclagutierrez@gmailcom>
2022-04-19 11:42:26 -0500

    se termino de agregar los servicios

--- routes/rutas.js
+++ routes/rutas.js
@@ -17,6 +17,56 @@ rutas.post('/api/v1/habitaciones/', function (req, res) {
   rutas.get('/api/v1/habitaciones/', function (req, res) {
     res.send('Hello World')
   })
+
+
+  //3.Servicio para buscar por id las habitaciones
+
+
+  rutas.get('/api/v1/habitaciones/id/', function (req, res) {
+    res.send('Hello World')
+  })
+
   
+  //4. Servicio para Cambiar la habitacion
+
+  rutas.put('/api/v1/habitaciones/', function (req, res) {
+    res.send('Hello World')
+  })
+
+  //5. Servicio para eliminar la  habitacion
+
+
+  rutas.delete('/api/v1/habitaciones/', function (req, res) {
+    res.send('Hello World')
+  })
+
 
 //****************** */ RUTAS PARA LOS SERVICIOS DE RESERVA*******************************
+
+//1.Servicio agregsr Reserva
+
+rutas.post('/api/v1/reservas/', function (req, res) {
+    res.send('Hello World')
+  })
+  
+  //2.Servicio para buscar  reservas por ID
+
+  rutas.get('/api/v1/reservas/id/', function (req, res) {
+    res.send('Hello World')
+  })
+
+
+  //3.Servicio para editar reserva
+
+
+  rutas.put('/api/v1/reservas/', function (req, res) {
+    res.send('Hello World')
+  })
+
+  
+  //4. Servicio para Cambiar la habitacion
+
+  rutas.delete('/api/v1/reservas/', function (req, res) {
+    res.send('Hello World')
+  })
+
